15d6287a273d863b39ddb002283ef31250bb8223
seunghabaik <slug0326@naver.com>
2022-01-09 13:32:35 +0900

    회원가입 및 회원목록 controller개발

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -39,7 +39,7 @@ public class MemberService {
     /**
      * 회원조회
      */
-    public List<Member> findAll() {
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
